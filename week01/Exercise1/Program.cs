053f9ec3765702461949240cdf438aec3dfc648c
Samuel Christian Asian <sammyasian159@gmail.com>
2026-05-07 14:12:24 +0100

    I completed the Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        String fname = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        String lname =  Console.ReadLine();
+        Console.WriteLine($"Your is is {lname}, {fname} {lname}.");
+        
+
     }
 }
