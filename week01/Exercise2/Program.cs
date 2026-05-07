000d0403a8b82e3ef460938d651e21f0bfd5c0d7
Samuel Christian Asian <sammyasian159@gmail.com>
2026-05-07 16:09:40 +0100

    Assignment Completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Please Enter your Grade Percentage: ");
+        String gradePercentage = Console.ReadLine();
+        int number = int.Parse(gradePercentage);
+        string letter = "";
+        
+        if (number >= 90 )
+        {
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (number >=70 )
+        {
+            Console.WriteLine("You Passed the course.. Congratulations");
+        }
+        else
+        {
+            Console.WriteLine("You did not meet the required Grade, Try Again next time!");
+        }
+
     }
 }
