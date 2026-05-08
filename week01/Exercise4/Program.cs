36e17554e3223f0663a9e4b1cd824f5c85914f0b
Samuel Christian Asian <sammyasian159@gmail.com>
2026-05-08 15:00:29 +0100

    Completed the assignments

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,46 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List <int> numbers = new List<int>();
+        int userInput = -1;
+        while (userInput !=0)
+        {
+            Console.WriteLine("Enter series of number, Type 0 to stop");
+            string userResponse = Console.ReadLine();
+            userInput= int.Parse(userResponse);
+
+            if (userInput != 0)
+            {
+               numbers.Add(userInput); 
+            }
+        }
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+
+        }
+         Console.WriteLine($"The sum is: {sum}");
+
+         // Calculating for Average
+         float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers [0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+
+            
+
+        }
+        Console.WriteLine($"The max is: {max}");
     }
+    
 }
